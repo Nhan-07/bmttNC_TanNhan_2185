057be61071b444f350811e2677020abf5596a7a0
Nhan-07 <phantannhan0701@gmail.com>
2025-05-28 21:19:04 +0700

    [add] Bai2 railfence

--- Bai2/cipher/railfence/apirailfence.py
+++ Bai2/cipher/railfence/apirailfence.py
@@ -1,8 +1,13 @@
-from cipher.railfence import RailFenceCipher
+from flask import Flask, request, jsonify
+from cipher.railfence import RailFenceCipher  # Thêm phần đầu file
+
+app = Flask(__name__)
+
+# RAILFENCE CIPHER ALGORITHM
 railfence_cipher = RailFenceCipher()
 
 @app.route('/api/railfence/encrypt', methods=['POST'])
-def railfence_encrypt():
+def encrypt():
     data = request.json
     plain_text = data['plain_text']
     key = int(data['key'])
@@ -10,9 +15,12 @@ def railfence_encrypt():
     return jsonify({'encrypted_text': encrypted_text})
 
 @app.route('/api/railfence/decrypt', methods=['POST'])
-def railfence_decrypt():
+def decrypt():
     data = request.json
     cipher_text = data['cipher_text']
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
